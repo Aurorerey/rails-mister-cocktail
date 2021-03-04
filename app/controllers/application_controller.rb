fd686dce8a30b5b4e00ad736c554c1b196509b59
Aurore Rey <rey.pandora@gmail.com>
2021-03-04 15:43:28 +0100

    first controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+      if @cocktail.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
 end
